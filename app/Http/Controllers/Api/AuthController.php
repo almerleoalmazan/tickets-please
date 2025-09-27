98173d09b13a8de582e5c9736b1d1b118a49e29a
Almer Leo Almazan <almerleoalmazan@gmail.com>
2025-09-27 16:00:24 +0800

    feat: create register route

--- app/Http/Controllers/Api/AuthController.php
+++ app/Http/Controllers/Api/AuthController.php
@@ -3,9 +3,11 @@
 namespace App\Http\Controllers\Api;
 
 use App\Http\Controllers\Controller;
+use App\Http\Requests\ApiLoginRequest;
 use App\Traits\ApiResponses;
 use Illuminate\Http\JsonResponse;
 use Spatie\RouteAttributes\Attributes\Get;
+use Spatie\RouteAttributes\Attributes\Post;
 use Spatie\RouteAttributes\Attributes\Prefix;
 
 #[Prefix('/v1')]
@@ -13,9 +15,15 @@ class AuthController extends Controller
 {
     use ApiResponses;
 
-    #[Get('/login')]
-    public function login(): JsonResponse
+    #[Post('/login')]
+    public function login(ApiLoginRequest $request): JsonResponse
     {
-        return $this->ok('Login successful');
+        return $this->ok($request->get('email'));
+    }
+
+    #[Post('/register')]
+    public function register(): JsonResponse
+    {
+        return $this->ok('register');
     }
 }
